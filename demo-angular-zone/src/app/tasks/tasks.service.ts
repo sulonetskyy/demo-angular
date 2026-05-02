39e56ef21edf6b6253007ea4974a2d05e17a372e
sulonetskyy <sergiy.ulonetskyy@gmail.com>
2026-05-02 12:48:34 +0200

    add demo-angular-zone

---
+++ demo-angular-zone/src/app/tasks/tasks.service.ts
@@ -0,0 +1,39 @@
+import {DUMMY_TASKS} from '../dummy-tasks';
+import {NewTaskData} from "./task/task.model";
+import {Injectable} from "@angular/core";
+
+@Injectable({ providedIn: 'root' })
+export class TasksService {
+  private tasks = DUMMY_TASKS;
+
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
+  getUserTasks(userId: string) {
+    return this.tasks.filter(task => task.userId === userId);
+  }
+
+  addTask(taskData: NewTaskData, userId: string) {
+    this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      userId: userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date
+    });
+    this.saveTasks();
+  }
+
+  removeTask(id: string) {
+    this.tasks = this.tasks.filter(task => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+}
